beba026f9eba2e1435e8e0b17ba5051af20f7cca
mohmed fazil <mohmedfazil@lpu.in>
2025-03-05 18:28:26 +0530

    backend from local host

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -17,13 +17,13 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch('https://madurai-special.onrender.com/allproducts') 
+    fetch('http://localhost:3000/allproducts') 
           .then((res) => res.json()) 
           .then((data) => setProducts(data))
 
     if(localStorage.getItem("auth-token"))
     {
-      fetch('https://madurai-special.onrender.com/getcart', {
+      fetch('http://localhost:3000/getcart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -63,7 +63,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     if(localStorage.getItem("auth-token"))
     {
-      fetch('https://madurai-special.onrender.com/addtocart', {
+      fetch('http://localhost:3000/addtocart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -81,7 +81,7 @@ const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     if(localStorage.getItem("auth-token"))
     {
-      fetch('https://madurai-special.onrender.com/removefromcart', {
+      fetch('http://localhost:3000/removefromcart', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
